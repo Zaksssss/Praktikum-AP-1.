68c37d6255d7d5a6d8600732f385edd87de9434a
Zaksssss <145890742+Zaksssss@users.noreply.github.com>
2023-09-29 15:36:13 +0700

    Add files via upload

---
+++ pertemuan-2-soal-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  int a = 750;
+  int b = 20;
+  int hasil = a / b;
+
+  cout << "kecepatan kendaraan amir adalah = " << hasil << endl;
+  return 0;
+}
